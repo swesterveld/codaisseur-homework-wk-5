8943c4321559d52970e2a662100bf29f379146fe
Sil Westerveld <git@silwesterveld.com>
2018-11-17 23:42:07 +0100

    Make song unique (based on combination of title, artist, and album) to make it impossible to have identical songs on different playlists.

--- songs/model.js
+++ songs/model.js
@@ -5,14 +5,17 @@ const sequelize = require('../db')
 const Song = sequelize.define('songs', {
   title: {
     type: Sequelize.STRING,
+    unique: 'compositeIndex',
     allowNull: false
   },
   artist: {
     type: Sequelize.STRING,
+    unique: 'compositeIndex',
     allowNull: false
   },
   album: {
     type: Sequelize.STRING,
+    unique: 'compositeIndex',
     allowNull: false
   }
 }, {
