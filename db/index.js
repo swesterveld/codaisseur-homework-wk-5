f4432101c4524093ee08509f11b863a5a2cb8186
Sil Westerveld <git@silwesterveld.com>
2018-11-18 21:53:24 +0100

    Remove abstraction in db

--- db/index.js
+++ db/index.js
@@ -1,1 +1,12 @@
-module.exports = require('./pgsql') // supporting only one type of database (which is Postgres)
+const Sequelize = require('sequelize')
+
+const connectionURI = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
+const sequelize = new Sequelize(connectionURI)
+
+sequelize.sync()
+  .then(() => {
+    console.log('Sequelize updated database schema')
+  })
+  .catch(console.error)
+
+module.exports = sequelize
